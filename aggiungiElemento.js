a502d9b3e3b08333c910a58f83e0d85ea1482d01
ossena <alessandro.ossena@myenaip.eu>
2024-12-19 09:48:08 +0000

    AggiornamentiBottoni1

--- aggiungiElemento.js
+++ aggiungiElemento.js
@@ -1,3 +1,14 @@
 //Seleziona il bottone di aggiunta
 
 const bottoneAggiungi = document.getElementById('bottoneAggiungi');
+const bottoneTogli = document.getElementById('bottoneTogli');
+const lista = document.getElementById('miaLista');
+
+bottoneAggiungi.addEventListener('click', function () {
+
+    
+    const nuovoElemento = document.createElement('li');
+
+
+})
+
