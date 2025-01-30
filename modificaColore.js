cd83747ece9a9dc4d73d7a417ea118f8c9309acc
ossena <alessandro.ossena@myenaip.eu>
2025-01-30 07:56:40 +0000

    modificaColore e file styleCss

---
+++ modificaColore.js
@@ -0,0 +1,8 @@
+const box = document.getElementById('box');
+const pulsante = document.getElementById('CambiaColoreBtn');
+
+
+function coloreCasuale(){
+    const lettere = '0123456789ABCDEF';
+
+}
