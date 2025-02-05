8b0a9e6ca11fc705b370d3dffdc52ad576bfa4f0
ossena <alessandro.ossena@myenaip.eu>
2025-02-05 09:49:11 +0000

    while esercizio

---
+++ esercizioWhile.js
@@ -0,0 +1,21 @@
+let ripeti = 1000;
+let i = 0;
+
+
+//stampa la variabile ripeti per 5 volte 
+while (i < ripeti) {
+    console.log(ripeti);
+    i++;
+}
+
+//crea conto alla rovesca #1
+while (i < ripeti) {
+    console.log(ripeti);
+    ripeti--;
+}
+
+//conto alla rovesca #2
+while (i < ripeti) {
+    console.log(ripeti - i);
+    i++;
+}
---
+++ studioArray.js
@@ -0,0 +1,3 @@
+let elencoFrutta = ["arancia", "mela", "banana" ];
+
+console.log(elencoFrutta[1]);
