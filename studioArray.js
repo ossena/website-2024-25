d7d8f1df60dff88f50c54136eedad59103ac4c19
ossena <alessandro.ossena@myenaip.eu>
2025-02-06 07:59:49 +0000

    frutta

--- studioArray.js
+++ studioArray.js
@@ -1,3 +1,27 @@
-let elencoFrutta = ["arancia", "mela", "banana" ];
 
-console.log(elencoFrutta[1]);
+//l'array è una struttura di dati semplice per memorizare più valori
+
+let elencoFrutta = ["kiwi", "mela", "nespole" ];
+let array1 = [];
+
+
+
+console.log(elencoFrutta[0]);
+
+console.log("La lunghezza di elencoFrutta é:"+ elencoFrutta.length);
+console.log("La lunghezza di array1 é:"+ array1.length);
+
+//per accedere all'ultimo elemento
+let ultimoElementoFrutta = elencoFrutta[elencoFrutta.length - 1];
+
+//push aggiunge un elemento all'array
+elencoFrutta.push("cachi");
+console.log(elencoFrutta);
+
+console.log(ultimoElementoFrutta);
+
+
+
+
+
+
