21984dc8704cff2df0a49901b70803269226426f
ossena <alessandro.ossena@myenaip.eu>
2025-02-13 12:57:32 +0000

    anagrammi1.js

--- Anagrammi.js
+++ Anagrammi.js
@@ -1,19 +1,22 @@
 function sonoAnagrammi(parola1,parola2){
+    let parola1Clean = parola1.replace(/\s+/g, '').toLowerCase();
+    let parola2Clean = parola2.replace(/\s+/g, '').toLowerCase();
+    console.log(parola1Clean,parola2Clean);
+};
+p1 = "ciA u"
+p2 = "Bell o"
+sonoAnagrammi(p1,p2);
 
+/* function sonoAnagrammi(parola1,parola2){
 let p1 = "";
 let p2 = "";
-
 p1 = parola1;
 p2 = parola2;
-
 let lunghezzaParola1 = p1.length;
 let lunghezzaParola2 = p2.length;
-
 //confronto la lunghezza
     if (lunghezzaParola1 !== lunghezzaParola2){
         return false
     }
-
 };
-
-console.log(sonoAnagrammi("casa","arancia"));
+console.log(sonoAnagrammi("casa","arancia")); */
---
+++ anagrammi1.js
@@ -0,0 +1,8 @@
+function sonoAnagrammi(parola1,parola2){
+    let parola1Clean = parola1.replace(/\s+/g, '').toLowerCase();
+    let parola2Clean = parola2.replace(/\s+/g, '').toLowerCase();
+    console.log(parola1Clean,parola2Clean);
+};
+p1 = "ciA u"
+p2 = "Bell o"
+sonoAnagrammi(p1,p2);
