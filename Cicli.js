c7bbb84d0a164a157ec04d0720bdf42f3908e411
ossena <alessandro.ossena@myenaip.eu>
2025-03-06 08:59:38 +0000

    ciao

---
+++ Cicli.js
@@ -0,0 +1,16 @@
+let i = 0 ;
+
+while(i<6){
+    console.log(i);
+    i++;
+}
+
+
+//output -> 0 1 2 3 4 5 
+
+let vegetali = ["carciofi","barbabietole","zucchine","finocchi","carote","cetrioli","broccoli",""]
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
